ea8ab9bfcdcd39eded530a9d3c0f4b649e0b8f2e
Alexandre Girard <git@alexgirard.com>
2017-11-29 14:27:30 +0100

    migrate to sequelize

--- index.js
+++ index.js
@@ -2,144 +2,80 @@ const path = require('path');
 const fs = require('fs');
 const TelegramBot = require('node-telegram-bot-api');
 const env = require('node-env-file');
+const Sequelize = require('sequelize');
 
-const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('lester.db');
+const dbPath = 'lester.db';
 
 env(__dirname + '/.env');
 
-const initDatabase = () => {
-  db.serialize(function() {
-    db.run(`PRAGMA foreign_keys = ON`);
-    db.run(`CREATE TABLE IF NOT EXISTS users (
-      id integer PRIMARY KEY,
-      love_index INTEGER DEFAULT 0,
-      session_timestamp INTEGER DEFAULT 0
-    )`);
-    db.run(`CREATE TABLE IF NOT EXISTS loves (
-      id integer PRIMARY KEY AUTOINCREMENT,
-      content text NOT NULL,
-      sentiment integer DEFAULT 0
-    )`);
-    db.run(`CREATE TABLE IF NOT EXISTS user_loves (
-      user_id integer,
-      love_id integer,
-      FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE,
-      FOREIGN KEY(love_id) REFERENCES loves(id) ON DELETE CASCADE
-    )`);
-    db.run(`CREATE INDEX IF NOT EXISTS userindex ON user_loves(user_id)`);
-    db.run(`CREATE INDEX IF NOT EXISTS loveindex ON user_loves(love_id)`);
-  });
-}
-initDatabase();
+const sequelize = new Sequelize('database', 'username', 'password', {
+  host: 'localhost',
+  dialect: 'sqlite',
 
-const insertLoveContent = (user_id, content) => {
-  db.run(`INSERT INTO loves (content) VALUES ($content)`,
-    {$content: content},
-    function(err) {
-      db.run(`INSERT INTO user_loves VALUES ($user_id, $love_id)`,
-        {$user_id: user_id, $love_id: this.lastID}
-      );
-    }
-  );
-}
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  },
 
-const initUser = (user_id, callback) => {
-  db.serialize(function() {
-    db.run(`INSERT INTO users (id) VALUES ($id)`,
-      {$id: user_id},
-      function() {
-        insertLoveContent(user_id, 'mom');
-        insertLoveContent(user_id, 'dad');
-        insertLoveContent(user_id, 'hater');
-      }
-    );
-  });
-  callback();
-}
+  storage: dbPath,
 
-const getCurrentLoveIndex = (user_id, callback) => {
-  db.get(`SELECT love_index FROM users
-    WHERE id = $user_id`,
-    {$user_id: user_id},
-    function(err, row) {
-      callback(row.love_index);
-    }
-  );
-}
+  // http://docs.sequelizejs.com/manual/tutorial/querying.html#operators
+  operatorsAliases: false
+});
 
-const setCurrentLoveIndex = (user_id, love_index, callback) => {
-  db.get(`SELECT count(*) count FROM user_loves
-    WHERE user_id = $user_id`,
-    {$user_id: user_id},
-    function(err, row) {
-      love_index += 1;
-      if(love_index == row.count) {
-        love_index = 0;
-      }
-      db.run(`UPDATE users SET love_index = $love_index WHERE id = $user_id`,
-             {$love_index: love_index, $user_id: user_id},
-             () => {callback(love_index)})
-    }
-  );
-}
+const User = sequelize.define('user', {
+  id: {type: Sequelize.INTEGER, primaryKey: true},
+  love_index: {type: Sequelize.INTEGER, defaultValue: 0}
+});
 
-const fetchUserLove = (user_id, callback) => {
-  db.serialize(function() {
-    getCurrentLoveIndex(user_id, (love_index) => {
-      setCurrentLoveIndex(user_id, love_index, (love_index) => {
-        db.get(`SELECT * FROM loves l
-               LEFT JOIN user_loves ul ON ul.love_id = l.id
-               WHERE ul.user_id = $user_id
-               LIMIT $love_index, 1`,
-               {$love_index: love_index, $user_id: user_id},
-               function(err, row) {
-                 callback(row.content);
-               });
-      });
-    });
-  });
-}
+const Love = sequelize.define('love', {
+  id: {type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true},
+  content: {type: Sequelize.TEXT, allowNull: false},
+  sentiment: {type: Sequelize.INTEGER, defaultValue: 0},
+  status: {type: Sequelize.INTEGER, defaultValue: -1}
+});
 
-const emptyUserLove = (user_id, callback) => {
-  db.run(`DELETE FROM loves
-         WHERE id IN ( SELECT love_id FROM user_loves WHERE user_id = $user_id )`,
-         {$user_id: user_id},
-         function() {
-          db.run(`DELETE FROM user_loves WHERE user_id = $user_id`,
-                 {$user_id: user_id},
-                 callback);
-         });
-}
+User.belongsToMany(Love, {through: 'UserLove'});
+Love.belongsToMany(User, {through: 'UserLove'});
 
-const getLoveCount = (user_id, callback) => {
-  db.run(`SELECT count(user_id) AS count FROM user_loves WHERE user_id = $user_id`,
-         {$user_id: user_id},
-         function(err, row) {
-           callback(this.changes);
-         });
-}
+sequelize.sync()
 
 const importFile = (user_id, filepath, callback) => {
-  db.serialize(function() {
-    setCurrentLoveIndex(user_id, 0, (love_index) => {
-      emptyUserLove(user_id, () => {
-
+  User.findOne({
+    where: {id: user_id}
+  }).then(user => {
+    user.love_index = 0;
+    user.save({fields: ['love_index']}).then(() => {
+      user.setLoves([]).then(() => {
         let count = 0;
 
         var lineReader = require('readline').createInterface({
           input: require('fs').createReadStream(filepath)
         });
 
+        const loves = []
+
         lineReader.on('line', function (line) {
-          count += 1;
-          insertLoveContent(user_id, line);
+          loves.push({content: line});
         });
 
         lineReader.on('close', function () {
-          callback(count);
+          Love.bulkCreate(loves).then(() => {
+            Love.findAll({
+              where: {status: -1}
+            }).then(loves => {
+              user.setLoves(loves).then(() => {
+                Love.update(
+                  {status: 0},
+                  { where: {status: -1} }
+                );
+                callback(loves.length);
+              });
+            });
+          });
         });
-
       });
     });
   });
@@ -166,55 +102,58 @@ telegram_bot.on('message', (msg) => {
   const user_id = msg.from.id;
 
   if(msg.document && msg.document.file_id) {
+
     telegram_bot.sendMessage(chatId, `Importing file...`);
     const downloadPath = path.join(path.dirname(process.mainModule.filename), 'download');
-    telegram_bot.downloadFile(msg.document.file_id, downloadPath)
-      .then((filepath) => {
-        db.serialize(function() {
-          importFile(user_id, filepath, (count) => {
-            telegram_bot.sendMessage(chatId, `File imported - ${count} names`);
-          });
-        });
+
+    telegram_bot.downloadFile(msg.document.file_id, downloadPath).then((filepath) => {
+      importFile(user_id, filepath, (count) => {
+        telegram_bot.sendMessage(chatId, `File imported - ${count} names`);
       });
-  }
-
-
-  db.get(`SELECT * FROM users WHERE id=$id`,
-    {$id: user_id},
-    (err, row) => {
-
-      if(typeof(row) == 'undefined') {
-        //welcome message
-        initUser(user_id, () => {
-          let keyboard = [
-            {text: 'start', callback_data: 'start'},
-          ]
-
-          const opts = {
-            reply_to_message_id: msg.message_id,
-            reply_markup: JSON.stringify({
-              inline_keyboard: [keyboard]
-            })
-          };
-
-          telegram_bot.sendMessage(chatId, "User created", opts);
-        });
-      } else {
-        //start session?
-        let keyboard = [
-          {text: 'start', callback_data: 'start'},
-        ]
-
-        const opts = {
-          reply_markup: JSON.stringify({
-            inline_keyboard: [keyboard]
-          })
-        };
-
-        telegram_bot.sendMessage(chatId, "I know you, would you like to start?", opts);
-      }
     });
 
+  } else {
+
+    let keyboard = [
+      {text: 'start', callback_data: 'start'},
+    ]
+
+    const opts = {
+      reply_markup: JSON.stringify({
+        inline_keyboard: [keyboard]
+      })
+    };
+
+    User.findOne({
+      where: {id: user_id}
+    }).then(user => {
+
+      let message = "I know you, would you like to start?";
+
+      if(!user) {
+
+        User.create({
+          id: user_id,
+          loves: [
+            {content: 'mom'},
+            {content: 'dad'},
+            {content: 'hater'}
+          ]
+        }, {
+          include: [ Love ]
+        }).then(user => {
+          message = "User created";
+          telegram_bot.sendMessage(chatId, message, opts);
+        });
+
+      } else {
+        telegram_bot.sendMessage(chatId, message, opts);
+      }
+
+
+    });
+
+  }
 
 });
 
@@ -232,23 +171,21 @@ telegram_bot.on('callback_query', function onCallbackQuery(callbackQuery) {
       inline_keyboard: [keyboard]
     })
   };
-  db.get(`SELECT * FROM users WHERE id=$id`,
-    {$id: user_id},
-    (err, row) => {
 
-      if(typeof(row) == 'undefined') {
-        //welcome message
-        initUser(user_id, () => {
-          fetchUserLove(user_id, (content) => {
-            telegram_bot.sendMessage(msg.chat.id, content, opts);
-          });
-        });
+  User.findOne({
+    where: {id: user_id}
+  }).then(user => {
+    if(!user)
+      return null;
+
+    user.getLoves().then(loves => {
+      telegram_bot.sendMessage(msg.chat.id, loves[user.love_index].content, opts);
+      if(user.love_index + 1 >= loves.length) {
+        user.update({love_index: 0}).then(() => {})
       } else {
-        fetchUserLove(user_id, (content) => {
-          telegram_bot.sendMessage(msg.chat.id, content, opts);
-        });
+        user.increment('love_index');
       }
-    }
-  );
-});
+    });
+  });
 
+});
